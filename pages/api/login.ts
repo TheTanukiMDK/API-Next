bb6619ea7c4d06ea3ddd7e5291c921e08dc98921
TheTanukiMDK <riquicraft04@gmail.com>
2024-10-14 14:21:20 -0500

    Conexion a base de datos

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,27 +2,14 @@ import type { NextApiRequest ,NextApiResponse } from "next";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { error } from "console";
+import { PrismaClient } from "@prisma/client";
 
-interface User {
-    id:1;
-    username:string;
-    password:string;
-}
+const prisma = new PrismaClient()
 
 interface LoginResponse{
     token?: string;
     error?: string;
 }
-
-//Registros (Simulacion de base de datos)
-
-const users:User[] = [
-    { 
-    id: 1,
-    username: "PABLO2",
-    password: "1234",
-    }
-]
 
 export default async function handler(
     req: NextApiRequest,
@@ -34,10 +21,12 @@ export default async function handler(
         const {username, password} = req.body
 
         //buscar el usuario en base de datos (arreglo)
-        const userBd = users.find((u) => u.username === username)
-        
+        //const userBd = users.find((u) => u.username === username)
+        const user = await prisma.user.findUnique({
+            where: {username}
+        })
 
-        if (!userBd){
+        if (!user){
             return res.status(401).json({error:"El usuario no existe en la base dde datos"})
         }
         //const isPasswordValid = await bcrypt.compare(password, userBd.password)
@@ -48,9 +37,9 @@ export default async function handler(
 
         //generar el token JWT
         const token = jwt.sign(
-            {userId: userBd.id, userName: userBd.username},
+            {userId: user.id, username: user.username},
             process.env.JWT_SECRET as string,
-            {expiresIn: '50s'}
+            {expiresIn: '1h'}
         );
         return res.status(200).json({token})
     }
